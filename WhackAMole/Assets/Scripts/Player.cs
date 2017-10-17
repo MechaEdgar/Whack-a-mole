a9085ecd7b6c2d1769c20a42aa9169b14c591244
MechaEdgar <ee1206@hotmail.com>
2017-10-16 21:06:31 -0500

    Change the Gvr Sdk.

---
+++ WhackAMole/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GvrViewer.Instance.Triggered || Input.GetKeyDown("space")){
+		RaycastHit hit;
+
+		if(Physics.Raycast(transform.position,transform.forward, out hit)){
+				if(hit.transform.GetComponent<Mole>() != null){
+					Debug.Log ("Hit a mole");
+					
+				}
+			}
+		}
+	}
+}
